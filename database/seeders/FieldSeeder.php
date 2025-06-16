acd00f3e9c5deadc7e3fe33390528c8e661ae450
Arjunstein <arjun.gnw09@gmail.com>
2025-06-16 14:28:14 +0700

    🍻 create dummy fields

---
+++ database/seeders/FieldSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class FieldSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+
+        foreach (range(1, 10) as $index) {
+            DB::table('fields')->insert([
+                'id' => (string) \Illuminate\Support\Str::uuid(),
+                'field_name' => 'Field Arena ' . $faker->word . ' ' . $index,
+                'description' => $faker->sentence,
+                'price_day' => $faker->numberBetween(120000, 150000),
+                'price_night' => $faker->numberBetween(170000, 250000),
+                'status' => $faker->randomElement(['Ready', 'In Use']),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
